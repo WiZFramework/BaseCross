af9f95e8ce42669f4dd1a5b36c6e921832629d0e
YamanoiYasushi <wiz.yamanoi@wiz.ac.jp>
2016-07-25 16:11:40 +0900

    単純なWindowsアプリケーションの作成

---
+++ SimplSampleTest/BaseCrossDx11/WinMain.cpp
@@ -0,0 +1,17 @@
+
+#include "stdafx.h"
+
+//--------------------------------------------------------------------------------------
+//	int APIENTRY _tWinMain();
+//	用途: エントリポイント
+//--------------------------------------------------------------------------------------
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+
+	MessageBox(nullptr, L"テスト", L"テスト", MB_OK);
+
+	return 0;
+}
---
+++ SimplSampleTest/BaseCrossDx11/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+// Windows ヘッダー ファイル:
+#include <windows.h>
+
+// UNICODE環境で必要なヘッダ
+#include <tchar.h>
+
+
